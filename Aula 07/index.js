d418debd1131a8c77e216fc00c4b2554f69b59fe
LiviaDevolder <liviadevolder@gmail.com>
2021-06-24 19:54:00 -0300

    Aula 08

--- Aula 07/index.js
+++ Aula 07/index.js
@@ -13,6 +13,10 @@ app.get("/blog", function(req, res) {
     res.send("Bem-vindo ao meu blog!")
 })
 
+app.get('/ola/:nome/:cargo/:cor', function(req, res) {
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2>Seu cargo é: " + req.params.cargo + "</h2>" + "<h3>Sua cor favorita é: " + req.params.cor + "</h3>")
+})
+
 app.listen(8081, function() {
     console.log("Servidor rodando na url http://localhost:8081")
 })
