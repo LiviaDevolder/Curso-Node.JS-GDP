2372640244ccb6a1335b11c4bebb317057670c46
LiviaDevolder <liviadevolder@gmail.com>
2021-06-25 19:29:38 -0300

    Aula 20

--- Aula 19/index.js
+++ Aula 19/index.js
@@ -1,17 +1,21 @@
 const express = require("express")
 const app = express()
 const handlebars = require('express-handlebars')
-const Sequalize = require('sequalize')
+const Sequelize = require('sequelize')
 
 // Config
     // Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
     // Conexão com o banco de dados MySql
-    const sequalize = new Sequalize('test', 'root', 'livia3103', {
-        host: "localhost",
-        dialect: 'mysql'
-    })
+        const sequelize = new Sequelize('test', 'root', 'livia3103', {
+            host: "localhost",
+            dialect: 'mysql'
+        })
+// Rotas
+app.get('/cad', function(req, res) {
+    res.render('formulario')
+})
 
 
 app.listen(8081, function() {
