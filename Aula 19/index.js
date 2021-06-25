5b38162856b574e9119cf3a38d861cf6dc8cf1ac
LiviaDevolder <liviadevolder@gmail.com>
2021-06-25 19:55:41 -0300

    Aula 22

--- Aula 19/index.js
+++ Aula 19/index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 // Config
     // Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
     // Conexão com o banco de dados MySql
         const sequelize = new Sequelize('test', 'root', 'livia3103', {
             host: "localhost",
@@ -18,8 +22,9 @@ const Sequelize = require('sequelize')
     })
 
     app.post('/add', function(req, res) {
-        res.send('FORMULARIO RECEBIDO!')
+        res.send('Texto: ' + req.body.titulo + " Conteudo: " + req.body.conteudo)
     })
+
 
 app.listen(8081, function() {
     console.log("Servidor rodando na url http://localhost:8081")
